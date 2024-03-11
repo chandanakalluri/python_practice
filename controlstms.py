07380d278ff2e93797862367037225fb37e1098f
chand796 <kalluri.chandana@diggibyte.com>
2024-03-11 15:52:53 +0530

    control and conditional stmts

---
+++ controlstms.py
@@ -0,0 +1,22 @@
+"""n=[1,3,4,5,6]
+print("even num")
+for i in n:
+	if i%2==0:
+		print(i)"""
+
+"""li=[50,60,60,7]
+#printing every ele
+i=0
+while(i<4):
+	print(li[i])
+	i=i+1"""
+"""li=[1,2,3,4]
+n=int(input())
+for i in li:
+	if i==n:
+		print("key is found")
+		break"""
+
+
+
+
